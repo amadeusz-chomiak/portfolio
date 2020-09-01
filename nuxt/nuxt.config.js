218cd120b8c4c95cd2260911d91c693597349012
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-01 13:37:40 +0200

    add nuxt typed vuex

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -50,6 +50,7 @@ export default {
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/composition-api',
+    'nuxt-typed-vuex',
   ],
   /*
    ** Nuxt.js modules
