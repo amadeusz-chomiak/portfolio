32c6454065a2af070aa596c8b1e5817a713a8679
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-02 20:46:48 +0200

    add Header basic implementation

--- nuxt/components/TheHeader.vue
+++ nuxt/components/TheHeader.vue
@@ -1,15 +1,37 @@
 <template>
-  <div></div>
+  <header class="flex items-center">
+    <div class="mr-4 flex-shrink-0">
+      <img class="rounded-full h-20" :src="headerPicture" :alt="headerAlt" />
+    </div>
+    <div>
+      <h1 class="inline-flex flex-wrap items-baseline">
+        <span
+          class="font-bold text-3xl text-primary-500 block flex-shrink-0 mr-2"
+          >{{ headerTitle }}</span
+        >
+        <span
+          class="italic text-2xl text-opacity-75 text-primary-100 block flex-shrink-0"
+          >{{ headerSubtitle }}</span
+        >
+      </h1>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
-import { ref, reactive, defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, computed } from '@nuxtjs/composition-api'
+import { useQuerySite, useImage } from '~/composable/useDatabase'
 
-
-export default defineComponent ({
+export default defineComponent({
   setup() {
+    const { result } = useQuerySite()
+    const header = computed(() => result.value?.Header)
+    const headerPicture = useImage(header.value?.Image, 224)
+    const headerAlt = computed(() => header.value?.Image?.Alt?.pl)
 
-    return {}
-  }
+    const headerTitle = computed(() => header.value?.Title?.pl)
+    const headerSubtitle = computed(() => header.value?.Subtitle?.pl)
+    return { headerAlt, headerPicture, headerTitle, headerSubtitle }
+  },
 })
 </script>
--- nuxt/test/unit/TheHeader.spec.ts
+++ nuxt/test/unit/TheHeader.spec.ts
@@ -1,11 +1,29 @@
-import { fireEvent } from '@testing-library/vue'
-import { Base } from '../utils/core'
+// import { fireEvent } from '@testing-library/vue'
+import { Base, useQuerySite } from '../utils/core'
 import Component from '~/components/TheHeader.vue'
 const base = new Base(Component)
+const { result } = useQuerySite()
 
 describe('components/TheHeader.vue', () => {
-  test.todo('implement tests!')
-  test('', async () => {
+  test('Display avatar image', () => {
+    const { getByAltText } = base.render()
+
+    const Image = getByAltText(
+      result.value?.Header?.Image?.Alt?.pl ?? ''
+    ) as HTMLImageElement
+    expect(Image).toBeVisible()
+    expect(Image.src).toContain('https://cdn.sanity.io/images/')
+  })
+
+  test('Display Title', () => {
     const { getByText } = base.render()
+    const Title = getByText(result.value?.Header?.Title?.pl ?? '')
+    expect(Title).toBeVisible()
+  })
+
+  test('Display Subtitle', () => {
+    const { getByText } = base.render()
+    const Subtitle = getByText(result.value?.Header?.Subtitle?.pl ?? '')
+    expect(Subtitle).toBeVisible()
   })
 })
