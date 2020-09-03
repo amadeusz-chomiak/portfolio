38a87fd54d3c1bbddf2cbfbfb126c501d676fef3
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-03 13:32:14 +0200

    set layout to switch to open menu on large screen

--- nuxt/components/TheNavigation.vue
+++ nuxt/components/TheNavigation.vue
@@ -1,15 +1,15 @@
 <template>
-  <div></div>
+  <div
+    class="w-full flex-grow-0 flex-shrink-0 lg:w-64 h-24 lg:h-full bg-primary-300"
+  ></div>
 </template>
 
 <script lang="ts">
 import { ref, reactive, defineComponent } from '@nuxtjs/composition-api'
 
-
-export default defineComponent ({
+export default defineComponent({
   setup() {
-
     return {}
-  }
+  },
 })
 </script>
--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="bg-black grid">
-    <TheHeader />
-    <TheNavigation />
-    <main>
-      <Nuxt />
-    </main>
+  <div class="bg-black flex flex-col h-screen">
+    <div class="m-4 md:m-5 lg:mt-10 lg:mb-6 lg:mx-8 xl:mt-12 xl:mb-8 xl:mx-10">
+      <TheHeader />
+    </div>
+    <div class="flex flex-col-reverse lg:flex-row flex-1">
+      <div class="lg:ml-6 xl:ml-8">
+        <TheNavigation />
+      </div>
+      <main
+        class="px-4 md:px-5 lg:px-6 lg:mt-10 lg:max-w-lg xl:px-8 xl:mt-12 xl:max-w-4xl flex-1 overflow-auto"
+      >
+        <Nuxt />
+      </main>
+    </div>
   </div>
 </template>
