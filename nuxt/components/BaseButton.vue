6b21c23ecca46c5ae041e37e1922a02325a1563c
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-04 15:00:00 +0200

    implement BaseButton

--- nuxt/components/BaseButton.vue
+++ nuxt/components/BaseButton.vue
@@ -1,15 +1,38 @@
 <template>
-  <div></div>
+  <component :is="rootTag" :href="target" :to="target" @click="$emit('click')">
+    <slot name="content">{{ props.content }}</slot>
+  </component>
 </template>
 
 <script lang="ts">
-import { ref, reactive, defineComponent } from '@nuxtjs/composition-api'
+import {
+  ref,
+  reactive,
+  defineComponent,
+  computed,
+} from '@nuxtjs/composition-api'
 
-
-export default defineComponent ({
-  setup() {
-
-    return {}
-  }
+export default defineComponent({
+  props: {
+    content: {
+      type: String,
+    },
+    target: {
+      type: [String, Object],
+    },
+    route: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const rootTag = computed(() =>
+      props.target ? (props.route ? 'nuxt-link' : 'a') : 'button'
+    )
+    return {
+      props,
+      rootTag,
+    }
+  },
 })
 </script>
--- nuxt/test/unit/BaseButton.spec.ts
+++ nuxt/test/unit/BaseButton.spec.ts
@@ -1,11 +1,33 @@
-import { fireEvent } from '@testing-library/vue'
 import { Base } from '../utils/core'
-import Component from "~/components/BaseButton.vue";
+import Component from '~/components/BaseButton.vue'
 const base = new Base(Component)
 
-describe("components/BaseButton.vue", () => {
-  test.todo("implement tests!")
-  test("", async () => {
-    const { getByText } = base.render()
-  });
-});
+const content = '__test content__'
+describe('components/BaseButton.vue', () => {
+  test('Show content prop', () =>
+    base.testPropInline((select) => select.getByText(content), { content }))
+  test('Show content slot', () => base.testHasSlot('content'))
+  describe('Root container', () => {
+    test('Root container is a "button", by default', () => {
+      base.render()
+      const Root = base.selectRoot()
+      expect(Root.tagName.toLowerCase()).toBe('button')
+    })
+
+    test('Root container emit click on click', () => base.testEmitter())
+
+    test('Root container is an "a" tag, if target is set', () => {
+      base.render({ props: { target: '#home' } })
+      const Root = base.selectRoot()
+      expect(Root.tagName.toLowerCase()).toBe('a')
+      expect(Root).toHaveAttribute('href', '#home')
+    })
+
+    test('Root container is a "nuxt-link", if "target" is set and "route" is true', () => {
+      base.render({ props: { target: 'pageAbout', route: true } })
+      const Root = base.selectRoot()
+      expect(Root.tagName.toLowerCase()).toBe('nuxt-link')
+      expect(Root).toHaveAttribute('to', 'pageAbout')
+    })
+  })
+})
