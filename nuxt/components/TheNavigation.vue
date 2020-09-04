10921013966d9316038108dc5069c2ec8feb7489
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-04 18:14:51 +0200

    add basic navigation links

--- nuxt/components/TheNavigation.vue
+++ nuxt/components/TheNavigation.vue
@@ -1,15 +1,40 @@
 <template>
-  <div
-    class="w-full flex-grow-0 flex-shrink-0 md:w-64 h-24 md:h-full bg-primary-300"
-  ></div>
+  <div class="w-full flex-grow-0 flex-shrink-0 h-24">
+    <div v-if="links">
+      <BaseButton
+        v-for="(link, index) in links"
+        :key="index"
+        :target="link.to || ''"
+        :content="link.title"
+        route
+        secondary
+        class="mb-4"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, reactive, defineComponent } from '@nuxtjs/composition-api'
+import {
+  ref,
+  reactive,
+  defineComponent,
+  computed,
+} from '@nuxtjs/composition-api'
+import { useQuerySite } from '~/composable/useDatabase'
 
 export default defineComponent({
   setup() {
-    return {}
+    const { result } = useQuerySite()
+    const pages = computed(() => result.value?.Pages)
+    const links = computed(() =>
+      pages.value?.map((page) => ({
+        title: page?.Title?.pl,
+        to: page?.Page?._id,
+      }))
+    )
+
+    return { links }
   },
 })
 </script>
--- nuxt/test/unit/TheNavigation.spec.ts
+++ nuxt/test/unit/TheNavigation.spec.ts
@@ -1,11 +1,28 @@
 import { fireEvent } from '@testing-library/vue'
-import { Base } from '../utils/core'
+import { Base, useQuerySite } from '../utils/core'
 import Component from '~/components/TheNavigation.vue'
 const base = new Base(Component)
+const { result } = useQuerySite()
 
 describe('components/TheNavigation.vue', () => {
-  test.todo('implement tests!')
-  test('', async () => {
+  test('Render site links', () => {
     const { getByText } = base.render()
+    const pages = result.value?.Pages
+    pages?.forEach((page) => {
+      const Link = getByText(page?.Title?.pl ?? '')
+      expect(Link).toBeVisible()
+    })
+  })
+
+  test('Render main link card', () => {
+    const { getByText } = base.render()
+    const page = result.value?.Pages?.find((page) => page?.Card)
+    const Button = getByText(page?.Card?.Action?.Primary?.pl ?? '')
+    expect(Button).toBeVisible()
+
+    const Content = getByText(
+      page?.Card?.Content?.pl?.[0]?.children?.[0]?.text ?? ''
+    )
+    expect(Content).toBeVisible()
   })
 })
