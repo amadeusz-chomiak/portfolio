bdd072752601383366a116e380881f2573132d1d
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-07 12:51:29 +0200

    add BaseContent prop content definition

--- nuxt/components/BaseContent.vue
+++ nuxt/components/BaseContent.vue
@@ -4,12 +4,17 @@
 
 <script lang="ts">
 import { ref, reactive, defineComponent } from '@nuxtjs/composition-api'
+import { Props } from '~/types/BaseContent'
 
-
-export default defineComponent ({
-  setup() {
-
+export default defineComponent<Props>({
+  props: {
+    content: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props) {
     return {}
-  }
+  },
 })
 </script>
---
+++ nuxt/types/BaseContent.ts
@@ -0,0 +1,47 @@
+type LiteralUnion<T extends U, U = string> = T | (U & never)
+
+interface LinkDef {
+  _key: string
+  _type: 'link'
+  href: string
+}
+
+interface RouteDef {
+  _key: string
+  _type: 'Route'
+  to: string
+}
+
+type Marks = LiteralUnion<'strong' | 'em'>
+interface Span {
+  _key: string
+  _type: 'span'
+  text: string
+  marks: Marks[]
+}
+
+type TextStyles = 'normal' | 'subtitle' | 'title' | 'unimportant'
+interface Block {
+  _key: string
+  _type: 'block'
+  children: Span[]
+  markDefs: (LinkDef | RouteDef)[]
+  style: TextStyles
+}
+
+type ListItemsStyles = 'bullet'
+
+interface BlockListItem extends Block {
+  level: number
+  listItem: ListItemsStyles
+}
+
+interface BlockDefinition {
+  _key: string
+  _type: 'Definition'
+  // todo implement
+}
+
+export interface Props {
+  content: (Block | BlockListItem)[]
+}
