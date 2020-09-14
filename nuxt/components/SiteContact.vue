b9f8c1365b3db4a7f4789d4604e6f4b69ad1869b
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-14 17:37:30 +0200

    hygen SiteContact

---
+++ nuxt/components/SiteContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <BaseContent id="SiteContact" :content="content" class="text-primary-50" />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, reactive, computed, defineComponent } from '@nuxtjs/composition-api'
+
+import {
+  useObserverProp,
+  useObserverObserve,
+  ObserverPropType,
+} from '~/composable/useObserver'
+interface Props extends ObserverPropType {
+  page: { content: { pl: unknown; en: unknown } }
+}
+
+export default defineComponent<Props>({
+  props: {
+    ...useObserverProp(),
+    page: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    useObserverObserve(props, SiteContact)
+
+    const content = computed(() => props.page.content.pl)
+    return { content }
+  }
+})
+</script>
