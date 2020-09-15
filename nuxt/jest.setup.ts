ecda7cbe3f60536da08306b744e5d0de16511f7f
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-09-15 11:37:11 +0200

    fix useContaxt throwing in jest tests

--- nuxt/jest.setup.ts
+++ nuxt/jest.setup.ts
@@ -1,3 +1,4 @@
+import { computed } from '@nuxtjs/composition-api'
 import '@testing-library/jest-dom'
 
 import Vue from 'vue'
@@ -29,3 +30,33 @@ Vue.component('ButtonIcon', ButtonIcon)
 
 //* The...
 Vue.component('TheNavigationContent', TheNavigationContent)
+
+jest.mock('@nuxtjs/composition-api', () => {
+  const originalModule = jest.requireActual('@nuxtjs/composition-api')
+
+  return {
+    __esModule: true, // Use it when dealing with esModules
+    ...originalModule,
+    useContext: jest.fn().mockReturnValue({
+      route: {
+        value: undefined,
+      },
+      query: {
+        value: undefined,
+      },
+      from: {
+        value: undefined,
+      },
+      params: {
+        value: undefined,
+      },
+    }),
+  }
+  //  return {
+  //    ...vm[globalNuxt].context,
+  //    route: computed(() => vm.$route),
+  //    query: computed(() => vm.$route.query),
+  //    from: computed(() => vm.$route.redirectedFrom),
+  //    params: computed(() => vm.$route.params),
+  //  }
+})
