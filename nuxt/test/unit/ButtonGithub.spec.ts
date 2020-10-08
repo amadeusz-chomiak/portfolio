13ce6a7fcea527be4b58c4fbbf92c597c7f92db8
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-08 13:26:41 +0200

    fix ButtonGithub test

--- nuxt/test/unit/ButtonGithub.spec.ts
+++ nuxt/test/unit/ButtonGithub.spec.ts
@@ -1,10 +1,11 @@
 import { Base } from '../utils/core'
 import Component from '~/components/ButtonGithub.vue'
+import { useIcon } from '~/composable/useIcon'
 const base = new Base(Component)
-
+const { alt } = useIcon('github')
 describe('components/ButtonGithub.vue', () => {
   test('Render content', () => {
-    const { getByText } = base.render()
-    expect(getByText('Mój Github')).toBeVisible()
+    const { getByAltText } = base.render()
+    expect(getByAltText(alt.value)).toBeVisible()
   })
 })
