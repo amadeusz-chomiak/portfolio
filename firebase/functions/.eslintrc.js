cb73731df2746312e55bd75f224b60e2de6dec94
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-10-21 14:50:56 +0200

    improve lint config

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -8,14 +8,14 @@ module.exports = {
     "plugin:import/errors",
     "plugin:import/warnings",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
-  },
+  // parser: "@typescript-eslint/parser",
+  // parserOptions: {
+  //   project: "tsconfig.json",
+  //   sourceType: "module",
+  // },
   plugins: [
     "@typescript-eslint",
-    "import",
+    "import", 
   ],
   rules: {
     "@typescript-eslint/adjacent-overload-signatures": "error",
