4ed22429fb7ebc6a77be3ad785d76d85da852348
amadeusz-chomiak <amadeuszchomiak@gmail.com>
2020-11-05 15:58:38 +0100

    add error layout

---
+++ nuxt/layouts/center.vue
@@ -0,0 +1,7 @@
+<template>
+  <main
+    class="bg-black h-screen w-full flex justify-center items-center content-center"
+  >
+    <Nuxt />
+  </main>
+</template>
---
+++ nuxt/layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <BaseCard background="circuit">
+    <template #content>
+      <div class="mb-4">
+        <h1
+          v-if="error.statusCode === 404"
+          class="text-xl font-semibold text-primary-950"
+        >
+          Niestety, ta strona nie istnieje
+        </h1>
+        <h1 v-else class="text-xl">Niestety, wystąpił problem</h1>
+      </div>
+    </template>
+    <template #action>
+      <BaseButton content="Wróć na stronę główną" route target="/" fill />
+    </template>
+  </BaseCard>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  props: ['error'],
+  layout: 'center',
+})
+</script>
